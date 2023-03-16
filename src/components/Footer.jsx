0268be4997aafcecacbfd3c3037fa3e3ce579330
Wenjun Zeng <wz165@duke.edu>
2023-03-15 21:22:55 -0400

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const date = new Date();
+
+function Footer(){
+    return(
+        <footer>
+            <p className="footer">Copyright © {date.getFullYear()}</p>
+        </footer>
+    );
+}
+
+export default Footer;
