8f889944ca2859db904adcbe2e78e4b4a91df54d
Wenjun Zeng <wz165@duke.edu>
2023-03-17 18:57:16 -0400

    map data into components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,28 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
+import notes from '../notes';
 
+//get the nodes from the notes.js file
+//map through the notes array
+//for each note, create a Note component
+//pass the title and content as props to the Note component
+//render the Note component
 function App(){
     return(
         <div>
             <Header />
-            <Note/>
+            {notes.map(noteItem => (
+                <Note
+                    key={noteItem.key}
+                    title={noteItem.title}
+                    content={noteItem.content}
+                />
+            ))}
             <Footer />
         </div>
     );
 }
+
 
 export default App;
